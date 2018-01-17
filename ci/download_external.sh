270a8247f544c00d068a60285280560d9e8bc2ed
arun-govindan <arun.govindan@lnttechservices.com>
2018-01-17 08:47:55 -0500

    test mac

--- ci/download_external.sh
+++ ci/download_external.sh
@@ -3,14 +3,14 @@ export DEPLOY_EXTERNAL_PATH=${DEPLOY_EXTERNAL_PATH:-http://96.116.56.119/externa
 REMOTE_HOST="$1"
 DEST_FILE="$2"
 LATEST_FILE=`curl -s $DEPLOY_EXTERNAL_PATH --list-only |sort -t\> -k8 | sed -n 's%.*href="\([^.]*\.tgz\)".*%\n\1%; ta; b; :a; s%.*\n%%; p' | tail -1`
-if [ -z "$LATEST_FILE"]
+if [ -z "$LATEST_FILE" ]
 then
   echo "****************** No Valid external present***************"
   exit 1;  
 else  
   echo "********* File to be downloaded: $LATEST_FILE********"
-  echo "******curl -s -o "$DEST_FILE/$LATETST_FILE" http://96.116.56.119/externals/$LATEST_FILE*****"
-  curl -o "$DEST_FILE/$LATETST_FILE" "http://96.116.56.119/externals/$LATEST_FILE"
+  echo "******curl -s -o "$DEST_FILE/$LATEST_FILE" http://96.116.56.119/externals/$LATEST_FILE*****"
+  curl -s -o "$DEST_FILE/$LATEST_FILE" "http://96.116.56.119/externals/$LATEST_FILE"
   #wget -q $DEPLOY_EXTERNAL_PATH -P $2
   if [ "$?" -ne 0 ]
   then
