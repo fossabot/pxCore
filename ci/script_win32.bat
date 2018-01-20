eb275c524db5ec5d9578892ec0bea5703b434f31
arun-govindan <arun.govindan@lnttechservices.com>
2018-01-20 12:19:14 -0500

    appveyor support

--- ci/script_win32.bat
+++ ci/script_win32.bat
@@ -1,10 +1,9 @@
 @rem this.bat file calls other batch files like unittest, testrunner etc.
-
 echo %CD%
-call ci\execute_win32.bat
+ci\execute_win32.bat
 
 echo ErrorLevel : %ERRORLEVEL%
 
-call execute_win32.bat
+execute_win32.bat
 
 
