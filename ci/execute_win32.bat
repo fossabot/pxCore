b20f910ff0c123136a6ae20ff27d4b3fef4a15ce
Govindan <arun.govindan@lnttechservices.com>
2018-01-23 14:42:43 -0500

    test appveyor

--- ci/execute_win32.bat
+++ ci/execute_win32.bat
@@ -4,7 +4,7 @@
 @rem
 
 set "PX_DUMP_MEMUSAGE=1"
-set "ENABLE_VALGRIND=1"
+@rem set "ENABLE_VALGRIND=1"
 set "RT_LOG_LEVEL=info"
 set "ORIG_DIR=%CD%"
 set "testRunner=https://px-apps.sys.comcast.net/pxscene-samples/examples/px-reference/test-run/testRunner.js"
@@ -15,31 +15,35 @@ set "BASE_DIR=%CD%"
 
 echo %CD%
 set "EXEC_LOG=%BASE_DIR%\logs\exec_logs.txt"
-cd build-win32\_CPack_Packages\win32\NSIS\pxscene-setup
+cd temp\_CPack_Packages\win32\NSIS\pxscene-setup
 
 start /B pxscene.exe %testRunner%?tests=%BASE_DIR%\tests\pxScene2d\testRunner\tests.json > %EXEC_LOG% 2>&1 &
 
+if %ERRORLEVEL% NEQ 0 (
+echo Failed to start pxscene.exe form current path : %CD%
+EXIT /B 0
+)
 
 findstr /C:"TEST RESULTS: " %EXEC_LOG%
 set errVal=%ERRORLEVEL%
 set count=0
+
 :while1
   if %errVal% NEQ 0 (
-    if %count% LSS 1500 (
-		set count+=30
-		timeout /t 30
+	if %count% LSS 1500 (
+		set /a count+=10
+		timeout /t 10
         findstr /C:"TEST RESULTS: " %EXEC_LOG%
-        set errVal=%ERRORLEVEL%		
-        goto while1:
-      )
+        set "errVal=%ERRORLEVEL%"		
+		goto while1:
+    )
   )
-
 taskkill /im pxscene.exe
 timeout /t 2
 
-findstr /C:"Failures: 0" %EXEC_LOG%
+findstr /C:"Failures: 0" "%EXEC_LOG%"
 if %ERRORLEVEL% NEQ 0 (
-    call:checkError "Testrunner failure" "Check the exec logs" "Follow the steps locally: export PX_DUMP_MEMUSAGE=1;export RT_LOG_LEVEL=info;./pxscene.sh $TESTRUNNERURL?tests=%BASE_DIR%\tests\pxScene2d\testRunner\tests.json locally and check for 'Failures: 0' in logs. Analyze whether failures is present or not"
+    call:checkError %ERRORLEVEL% "Testrunner failure" "Check the exec logs" "Follow the steps locally: export PX_DUMP_MEMUSAGE=1;export RT_LOG_LEVEL=info;./pxscene.sh $TESTRUNNERURL?tests=%BASE_DIR%\tests\pxScene2d\testRunner\tests.json locally and check for 'Failures: 0' in logs. Analyze whether failures is present or not"
 	cd %BASE_DIR%
 	EXIT /B 1
 )
@@ -47,18 +51,22 @@ if %ERRORLEVEL% NEQ 0 (
 
 
 cd %BASE_DIR%
+EXIT /B 0
 @rem type exec_log.txt
 
 :checkError
     echo. >>%EXEC_LOG%
 	echo. >>%EXEC_LOG%
     echo  ******************* >> %EXEC_LOG%
-    echo  Failure Reason :  %~1 >> %EXEC_LOG%
-	echo  Cause : %~2 >> %EXEC_LOG%
-	echo  Reproduction Procedure : %~3 >> %EXEC_LOG%
+    echo  Failure Reason :  %~2 >> %EXEC_LOG%
+	echo  Cause : %~3 >> %EXEC_LOG%
+	echo  Reproduction Procedure : %~4 >> %EXEC_LOG%
     echo  ******************* >> %EXEC_LOG%
 	echo. >>%EXEC_LOG%
 	echo. >>%EXEC_LOG%
+
+
+
 
 
 
