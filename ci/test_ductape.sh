0b0c3a4fae3153c7e099649150e68b2b53845295
arun-govindan <arun.govindan@lnttechservices.com>
2018-02-01 14:27:05 -0500

    test duktape

--- ci/test_ductape.sh
+++ ci/test_ductape.sh
@@ -3,7 +3,7 @@
 export DUKTAKE_SUPPORT=ON
 touch ~/.sparkUseDuktape
 echo "**************Enabling ducktape ******************* $?"
-rm -rf $TRAVIS_BUILD_DIR/temp/logs
+rm -rf $TRAVIS_BUILD_DIR/logs
 pwd
 sh $TRAVIS_BUILD_DIR/ci/duktape_install.sh
 
