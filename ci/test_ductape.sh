e640a0cb5a5a4b11dd619b6927c5c61a022e6578
arun-govindan <arun.govindan@lnttechservices.com>
2018-02-02 14:04:27 -0500

    test duktape

--- ci/test_ductape.sh
+++ ci/test_ductape.sh
@@ -5,7 +5,7 @@ touch ~/.sparkUseDuktape
 rm -rf $TRAVIS_BUILD_DIR/logs
 pwd
 
-function validateExe()
+validateExe()
 {
 if [ "$1" -ne 0 ]
 printf  "\n\n************** "$2" failed *******************\n\n"
@@ -19,7 +19,8 @@ validateExe "$?" "duktape_install.sh"
 sh $TRAVIS_BUILD_DIR/ci/script.sh
 validateExe "$?" "script.sh"
 
-if [0]
+if false ;
+then
 
 if [ "$TRAVIS_EVENT_TYPE" = "push" ] || [ "$TRAVIS_EVENT_TYPE" = "pull_request"]; then 
   codecov --build "$TRAVIS_OS_NAME-$TRAVIS_COMMIT-$TRAVIS_BUILD_NUMBER" -X gcov -f $TRAVIS_BUILD_DIR/tracefile ; 
