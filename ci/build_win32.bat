e7bb1ec645fe870f53ac82ddc8aaf468e748f659
Govindan <arun.govindan@lnttechservices.com>
2018-02-09 13:03:42 -0500

    test

--- ci/build_win32.bat
+++ ci/build_win32.bat
@@ -22,6 +22,12 @@ set "BASE_DIR=%CD%"
 set "VSCMD_START_DIR=%CD%"
 call "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build/vcvars32.bat" x86
 
+IF EXIST "C:\PuTTY\pscp.exe" (
+echo "-------------------------------------Yes" 
+) ELSE (
+echo "-------------------------------------No"
+)
+
 @rem build dependencies
 cd examples/pxScene2d/external
 call buildWindows.bat
