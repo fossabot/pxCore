3a2ca9dda72c666312c3173b56a953a268f66914
Govindan <arun.govindan@lnttechservices.com>
2018-02-10 22:36:54 -0500

    test

--- ci/after_build_win32.bat
+++ ci/after_build_win32.bat
@@ -13,10 +13,9 @@ cd %S%
 cd "build-win32/_CPack_Packages/win32/NSIS"
 
 
-
+if "%DUKTAPE_SUPPORT%" == "ON" (
   7z a -y pxscene-setup-exe.zip pxscene-setup.exe 
-  "C:\Program Files\PuTTY\pscp.exe" -i pxscene-build.pem.ppk build-win32/_CPack_Packages/win32/NSIS/pxscene-setup-exe.zip "ubuntu@96.116.56.119:/var/www/html/edge/windows"
-  
+  "C:\Program Files\PuTTY\pscp.exe" -i pxscene-build.pem.ppk pxscene-setup-exe.zip "ubuntu@96.116.56.119:/var/www/html/edge/windows"
   IF %ERRORLEVEL% NEQ 0 (
     echo "-------------------------- Failed to upload pxscene setup"
   )
@@ -24,7 +23,7 @@ cd "build-win32/_CPack_Packages/win32/NSIS"
   IF %ERRORLEVEL% NEQ 0 (
     echo "-------------------------- Failed to upload build logs"
   )
-
+)
 
 del pxscene-build.pem.ppk
 
