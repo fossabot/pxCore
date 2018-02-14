cd5d39ef8a6b1d46410a3abb3dd43bb71e5d7d53
Govindan <arun.govindan@lnttechservices.com>
2018-02-14 11:08:53 -0500

    test

--- ci/build_win32.bat
+++ ci/build_win32.bat
@@ -23,6 +23,7 @@ set "VSCMD_START_DIR=%CD%"
 call "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build/vcvars32.bat" x86
 
 cd C:\Qt\5.4\
+echo "!!!!!!!!!!!!!! %CD% !!!!!!!!!!!!!!!!!!!!!!"
 dir *opengl*.* /s
 
 cd %BASE_DIR%
