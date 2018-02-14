35dacc9f59ad94113e3edbd1fc1b5c2dff8c116d
Govindan <arun.govindan@lnttechservices.com>
2018-02-14 11:19:10 -0500

    test

--- ci/build_win32.bat
+++ ci/build_win32.bat
@@ -22,7 +22,7 @@ set "BASE_DIR=%CD%"
 set "VSCMD_START_DIR=%CD%"
 call "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build/vcvars32.bat" x86
 
-cd C:\Qt\5.4\
+cd C:\Qt
 echo "!!!!!!!!!!!!!! %CD% == %ERRORLEVEL% !!!!!!!!!!!!!!!!!!!!!!"
 dir *opengl*.* /s
 
