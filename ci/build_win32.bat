e87f37b19c0f5051a58ecc5084fae66b0347cb54
Govindan <arun.govindan@lnttechservices.com>
2018-02-14 10:35:44 -0500

    test

--- ci/build_win32.bat
+++ ci/build_win32.bat
@@ -22,6 +22,13 @@ set "BASE_DIR=%CD%"
 set "VSCMD_START_DIR=%CD%"
 call "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build/vcvars32.bat" x86
 
+cd C:\Qt\5.4\msvc2013_opengl
+tree
+
+cd C:\Qt\5.4\msvc2013_64_opengl
+tree
+
+cd %BASE_DIR%
 @rem build dependencies
 cd examples/pxScene2d/external
 call buildWindows.bat
